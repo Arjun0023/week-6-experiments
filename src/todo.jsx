b6fa5abb4206580c135e4e58f8676c6f11c15254
Arjun Pawar <108388620+Arjun0023@users.noreply.github.com>
2024-01-09 23:37:45 +0530

    Update todo.jsx

--- src/todo.jsx
+++ src/todo.jsx
@@ -18,6 +18,24 @@ function App(){
     )
 }
 
+function App(){
+  const [todos , setTodos] = useState([{
+      title:"Go to gym",
+      description:"need to hit the gym from 4-6PM"
+  },{
+      title:"Go to class",
+      description:"Need to go to class 7-9pm"
+  },{
+      title:"eat food",
+      description:"eat 9-10pm"
+  }])
+  return(
+      <div>
+      {todos.map(todo => <Todo Title={todo.title} description={todo.description}/>)}
+      </div>
+  )
+}
+
 function Todo({title, description}) {
     return <div>
       <h1>
